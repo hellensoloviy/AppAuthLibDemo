7f6d2862cf0a4676cd875fc1dbd4d7d8820e27a0
Hellen S <hellensoloviy@gmail.com>
2019-03-12 13:23:02 +0200

    add additional constants and another policy;

--- AppAuthLibDemo/Services/RequestsService.m
+++ AppAuthLibDemo/Services/RequestsService.m
@@ -11,7 +11,18 @@
 #import "AppDelegate.h"
 #import "Constants.h"
 
-static NSString * const kClientID = @"3e1745f6-e090-49c0-ba86-a4053506c686";
+///3e1745f6-e090-49c0-ba86-a4053506c686 - identity
+static NSString * const kClientIDIdentity = @"3e1745f6-e090-49c0-ba86-a4053506c686";
+
+static NSString * const kClientID = @"507235de-e1e0-4049-8fe0-63445a732096";
+static NSString * const kTenantID = @"1852f758-f68f-437f-b58b-346bcb2cbb03";
+
+static NSString * const kAuthorityURL = @"https://login.windows.net/\(tenantID)/oauth2/token";
+
+//https://login.microsoftonline.com/tfp/halsdev.onmicrosoft.com/B2C_1_SignUpInWeb/v2.0
+
+//@"https://login.microsoftonline.com/tfp/halsdev.onmicrosoft.com/B2C_1_SignUpInWeb/v2.0
+
 static NSString * const KRedirectURI = @"https://login.microsoftonline.com/halsdev.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_SignUpInAPI";
 
 @interface RequestsService()
@@ -27,7 +38,8 @@ static NSString * const KRedirectURI = @"https://login.microsoftonline.com/halsd
 
 - (void) performAuthFlowOnViewController: (UIViewController *) viewControllerToPresentOn {
     self.viewControllerToPresentOn = viewControllerToPresentOn;
-    NSURL *discoveryURL = [[NSURL alloc] initWithString: @"https://login.microsoftonline.com/halsdev.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_SignUpInAPI"];
+    NSURL *discoveryURL = [[NSURL alloc] initWithString: @"https://login.microsoftonline.com/halsdev.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_SignUpInWeb"];
+    //B2C_1_SignUpInWeb
     
     [OIDAuthorizationService discoverServiceConfigurationForDiscoveryURL: discoveryURL completion:^(OIDServiceConfiguration * _Nullable configuration, NSError * _Nullable error) {
         if (configuration != nil) {
@@ -45,12 +57,12 @@ static NSString * const KRedirectURI = @"https://login.microsoftonline.com/halsd
     // builds authentication request
     OIDAuthorizationRequest *request =
     [[OIDAuthorizationRequest alloc] initWithConfiguration:configuration
-                                                  clientId: kClientID
+                                                  clientId: kClientIDIdentity
                                                     scopes:@[OIDScopeOpenID,
                                                              OIDScopeProfile]
                                                redirectURL: redirectURL
                                               responseType: OIDResponseTypeCode
-                                      additionalParameters: @{@"p": @"B2C_1_SignUpInAPI"}];
+                                      additionalParameters: @{@"p": @"B2C_1_SignUpInWeb"}];
     
     // performs authentication request
     AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
